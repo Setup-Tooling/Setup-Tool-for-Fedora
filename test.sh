849a3a066325f72cd6ca8996f04e926895aabcc4
BurningPho3nix <95959450+BurningPho3nix@users.noreply.github.com>
2023-04-06 09:51:22 +0200

    Add files via upload

--- test.sh
+++ test.sh
@@ -24,22 +24,31 @@ do
             ;;
         "Install RPM Fusion Free")
             sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf groupupdate -y core
             ;;
         "Install RPM Fusion Free Tainted")
-            sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-tainted-$(rpm -E %fedora).noarch.rpm
+            sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+            sudo dnf groupupdate -y sound-and-video
+            sudo dnf install -y rpmfusion-free-release-tainted
             ;;
         "Install RPM Fusion Non-Free")
             sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf groupupdate -y core
             ;;
         "Install RPM Fusion Non-Free Tainted")
-            sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-tainted-$(rpm -E %fedora).noarch.rpm
+            sudo dnf install -y rpmfusion-nonfree-release-tainted
             ;;
         "Install All")
-            sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-tainted-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-tainted-$(rpm -E %fedora).noarch.rpm
+            sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf groupupdate -y core
+            sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+            sudo dnf groupupdate -y sound-and-video
+            sudo dnf install -y rpmfusion-free-release-tainted
+            sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf install -y rpmfusion-nonfree-release-tainted
             ;;
         "Update")
-            sudo dnf update -y
-            flatpak update -y
+            sudo dnf update -y && flatpak update -y
             ;;
         "Quit")
             break
