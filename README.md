45b43976a179100df51570a059d8c0a38ba17b0c
BurningPho3nix <pr@burningpho3nix.xyz>
2023-04-17 22:49:49 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# rpm-fusion-and-dnf setup (Fedora 38 compatible)
+
+This project helps you installing RPM-Fusion and making dnf.conf changes quickly on Fedora.
+
+_____________________________________________________
+
+pls read the README.md on older versions, if you use those, due to differences in the process
+______________________________________________________
+
+Script gives you the option to install:
+- RPM Fusion free (repo)
+- RPM Fusion nonfree (repo)
+- Group core (to enable downloads through software centers)
+- Group multimedia & Group sound-and-video (for more codecs)
+- RPM Fusion free tainted (repo)
+- libdvdcss (support for copyrighted DVDs)
+- RPM Fusion nonfree tainted (repo)
+- all of the above
+- system updates (dnf update)
+
+the script gives you the option reboot
+
+older versions still contained:
+- Flathub (repo) (removed in 38 due to it being enabled by default in Fedora 38)
+
+This project has no relation the projects named above.
+
+This needs to be enabled:
+![Screenshot from 2023-01-17 00-41-11](https://user-images.githubusercontent.com/95959450/212780926-f5806457-5b99-4c5c-9b70-ef21296ea32e.png)
+
+after enabling the above, right-click in the and open it in terminal then type in ./start.sh (if you are on Gnome just right-click the start.sh file and click "run as program")
---
+++ start.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+echo "Welcome to the my repository installer!"
+
+PS3='Please enter your choice: '
+options=("Modify dnf.conf" "Install RPM Fusion Free" "Install RPM Fusion Free Tainted" "Install RPM Fusion Non-Free" "Install RPM Fusion Non-Free Tainted" "DVD Compatibility" "Install All" "Update" "Quit" "Reboot system")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Modify dnf.conf")
+            echo "opening dnf modification"
+             if [ -f "dnf-mod.sh" ]; then
+                chmod +x dnf-mod.sh
+            sudo ./dnf-mod.sh
+            else
+            echo "The dnf-mod.sh script does not exist."
+            fi
+            ;;
+        "Install RPM Fusion Free")
+            sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf groupupdate -y core
+            if [ $? -eq 0 ]; then
+    echo "Successfully installed RPM"
+  else
+    echo "Failed to install RPM"
+    exit 1
+  fi
+            ;;
+        "Install RPM Fusion Free Tainted")
+            sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+            sudo dnf groupupdate -y sound-and-video
+            sudo dnf install -y rpmfusion-free-release-tainted
+            if [ $? -eq 0 ]; then
+    echo "Successfully installed RPMs"
+  else
+    echo "Failed to install RPMs"
+    exit 1
+  fi
+            ;;
+        "Install RPM Fusion Non-Free")
+            sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf groupupdate -y core
+            if [ $? -eq 0 ]; then
+    echo "Successfully installed RPM"
+  else
+    echo "Failed to install RPM"
+    exit 1
+  fi
+            ;;
+        "Install RPM Fusion Non-Free Tainted")
+            sudo dnf install -y rpmfusion-nonfree-release-tainted
+            if [ $? -eq 0 ]; then
+    echo "Successfully installed RPM"
+  else
+    echo "Failed to install RPM"
+    exit 1
+  fi
+            ;;
+            "DVD Compatibility")
+            sudo dnf install -y libdvdcss
+             if [ $? -eq 0 ]; then
+    echo "Successfully installed RPM"
+  else
+    echo "Failed to install RPM"
+    exit 1
+  fi
+            ;;
+        "Install All")
+            sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf groupupdate -y core
+            sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+            sudo dnf groupupdate -y sound-and-video
+            sudo dnf install -y rpmfusion-free-release-tainted
+            sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+            sudo dnf install -y rpmfusion-nonfree-release-tainted
+            sudo dnf install -y libdvdcss
+            if [ $? -eq 0 ]; then
+    echo "Successfully installed RPMs"
+  else
+    echo "Failed to install RPMs"
+    exit 1
+  fi
+            ;;
+        "Update")
+            sudo dnf update -y && flatpak update -y
+            if [ $? -eq 0 ]; then
+    echo "Successfully installed updates"
+  else
+    echo "Failed to install updates"
+    exit 1
+  fi
+            ;;
+        "Reboot system")
+      echo "Rebooting system..."
+      reboot
+            ;;
+        "Quit")
+        echo "Quitting script... See you next time :)"
+            break
+            ;;
+        *) echo "Invalid option $REPLY";;
+    esac
+done
+
+echo "Thank you for using the my repository installer! Goodbye."
+
