18aa19a9acccaa934fe99b5a03a0352fd2f5258b
BurningPho3nix <pr@burningpho3nix.xyz>
2023-04-28 00:54:59 +0200

    Update codec-setup.sh
    Signed-off-by: BurningPho3nix <pr@burningpho3nix.xyz>

--- codec-setup.sh
+++ codec-setup.sh
@@ -1,1 +1,4 @@
 
+sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
+            sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+            sudo dnf groupupdate -y sound-and-video
