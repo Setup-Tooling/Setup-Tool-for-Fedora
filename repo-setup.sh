53882c2850e848996f224d53fe4f7fcdc8b7ad7a
BurningPho3nix <pr@burningpho3nix.xyz>
2023-04-28 00:53:53 +0200

    Update repo-setup.sh
    Signed-off-by: BurningPho3nix <pr@burningpho3nix.xyz>

--- repo-setup.sh
+++ repo-setup.sh
@@ -20,9 +20,6 @@ do
             ;;
             
         "Install RPM Fusion Free Tainted")
-            sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
-            sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-            sudo dnf groupupdate -y sound-and-video
             sudo dnf install -y rpmfusion-free-release-tainted
             if [ $? -eq 0 ]; then
     echo "Successfully installed RPMs"
@@ -76,9 +73,6 @@ do
         "Install All")
             sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
             sudo dnf groupupdate -y core
-            sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
-            sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-            sudo dnf groupupdate -y sound-and-video
             sudo dnf install -y rpmfusion-free-release-tainted
             sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
             sudo dnf install -y rpmfusion-nonfree-release-tainted
