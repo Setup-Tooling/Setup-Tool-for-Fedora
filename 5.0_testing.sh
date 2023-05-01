0f489a828d06928c24a99c306b5ba92fb0c4a139
BurningPho3nix <pr@burningpho3nix.xyz>
2023-05-01 22:48:52 +0200

    Update 5.0_testing.sh
    Signed-off-by: BurningPho3nix <pr@burningpho3nix.xyz>

--- 5.0_testing.sh
+++ 5.0_testing.sh
@@ -163,12 +163,40 @@ case $OPTION in
         ;;
     4)
         ## PLACEHOLDER
-        if [ -f "package-installer.sh" ]; then
-         chmod +x package-installer.sh
-         sudo ./package-installer.sh
+        if command -v dnf &> /dev/null
+then
+    PACKAGE_MANAGER="dnf"
+elif command -v flatpak &> /dev/null
+then
+    PACKAGE_MANAGER="flatpak"
+else
+    echo "Error: Neither dnf nor flatpak is available on this system."
+    exit 1
+fi
+# Prompt the user to enter the packages they want to install
+read -p "Enter the names of packages you want to install (separated by spaces): " PACKAGES
+
+# Attempt to install each package using the appropriate package manager
+for PACKAGE in $PACKAGES
+do
+    echo "Trying to install $PACKAGE using $PACKAGE_MANAGER..."
+    if [ "$PACKAGE_MANAGER" = "dnf" ]
+    then
+        if dnf search -C $PACKAGE | grep -q "^$PACKAGE"
+        then
+            sudo dnf install $PACKAGE -y
+        elif command -v flatpak &> /dev/null
+        then
+            echo "$PACKAGE not found in system repositories, attempting to install using flatpak..."
+            flatpak install $PACKAGE -y
         else
-         echo "The package-installer.sh script does not exist."
+            echo "Error: $PACKAGE not found in system repositories, and flatpak is not available on this system."
         fi
+    elif [ "$PACKAGE_MANAGER" = "flatpak" ]
+    then
+        flatpak install $PACKAGE -y
+    fi
+done
        
         ;;
     5)
