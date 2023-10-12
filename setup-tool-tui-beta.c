4fd11f517a52dc64b070b94e2492404f82ac33f6
BurningPho3nix <pr@burningpho3nix.xyz>
2023-10-12 23:26:55 +0200

    Add files via upload
    Signed-off-by: BurningPho3nix <pr@burningpho3nix.xyz>

---
+++ setup-tool-tui-beta.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Make setup.sh executable
+    //system("chmod +x /usr/bin/setup.sh");
+
+    // Execute setup.sh
+    system("/usr/bin/setup.sh");
+
+    return 0;
+}
